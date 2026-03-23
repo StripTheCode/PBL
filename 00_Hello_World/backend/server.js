a8681a745d77b2b3ffdd3842c137b9c5453e782d
CodeStripper <stripthecode@gmail.com>
2026-03-23 19:08:29 +0530

    Add dotenv config for port

--- 00_Hello_World/backend/server.js
+++ 00_Hello_World/backend/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
     res.send("<h1> Hello World from <u>StripTheCode</u></h1>");
